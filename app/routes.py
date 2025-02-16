e1fccc054af8e58d689852249f0bef21ab0f1fff
death-and-taxes <dylana.crites@gmail.com>
2025-02-16 04:12:31 -0500

    new file:   routes.py 	modified:   ../__pycache__/cursd-blog.cpython-310.pyc

---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import render_template
+from app import app
+
+# @appvar.route are decorators, which associate urls '/' and '/index' with the index() function
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Randell'}
+    posts = [
+        {
+            'author' : {'username':'Johnny Smells'},
+            'body': 'Aint no smells in here'
+        },
+        {
+            'author': {'username':'SusAnne'},
+            'body': 'Whoops, dropped my hell pith'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+"""
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1> Howdy, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+"""
