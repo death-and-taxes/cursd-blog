c3773f98316f95a033e5f42a2d74e9b611bc86a2
death-and-taxes <dylana.crites@gmail.com>
2025-02-17 01:55:07 -0500

    removed duplicate login function definitions

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask import render_template, flash, redirect
+
 """
 each app route describes what the server will send in response to a request including
 the path specified in the arguements.
@@ -39,7 +41,32 @@ Before /templates/ was created, this is what was on this page.
 """
 
 
-@app.route('/login')
+'''
+The following is what allows the login to accept information
+'''
+#from flask import render_template, flash, redirect
+@app.route('/login', methods=['GET', 'POST']) #set methods to accept get and post instead of just get
 def login():
     form = LoginForm()
+
+#this if statement is what stops the browser from submitting when it initially requests the web page
+#if form.validate_on_submit returns true, two functions pass flash and redirect.
+    if form.validate_on_submit():
+        #flash function is a way to show users a message, this is temporary
+        #flashed messages do not magically appear, see base.html for how to layout flashed messages
+        flash('Oh? {} you are trying to login, remember_me={}'.format(form.username.data, form.remember_me.data))
+        #redirect automatically instructs the client to navigate back to the specified page
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+'''
+-------------------
+This is the older login function.
+-------------------
+#from app.forms import LoginForm
+@app.route('/login')
+def login():
+
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+'''    
