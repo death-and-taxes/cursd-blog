fe9be1772c57fe0f849320b92ed7b181f93afa10
death-and-taxes <dylana.crites@gmail.com>
2025-02-17 00:20:35 -0500

    updated forms.py with the LoginForm class

---
+++ forms.py
@@ -0,0 +1,13 @@
+'''
+This file stores web form classes. See Grinberg chap 3 
+'''
+from flask_wtf import FlaskForm
+from wtforms import String Field, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username"' validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    
