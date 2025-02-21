f5058fe59726bc74c456460cf5c49211d86fdd2c
death-and-taxes <dylana.crites@gmail.com>
2025-02-20 20:10:57 -0500

    added basedir variable to config.py

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ that are defined as keys.
 SECRET_KEY is a variable used to prevent cross-site request forgery
 '''
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'place-holder-pass-key-should-change-later'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
